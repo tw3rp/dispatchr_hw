430fd6132685050f3a34bbbbd74e4d734081b1cb
Rahul <rahuldprabhu@gmail.com>
2016-05-11 20:13:42 -0700

    added adapter and facade

---
+++ design_patterns/adapter.js
@@ -0,0 +1,11 @@
+var translate = {
+  regc: function(id){
+    //return tree in a scpecific format
+    return tree
+  }
+  ...
+  map: function(id){
+    //return tree in a different format
+    return tree
+  }
+}
---
+++ design_patterns/facade.js
@@ -0,0 +1,10 @@
+var Tree = function(id){
+  this.id = id;
+}
+
+tree.protoype = {
+  getTree: function(),
+  updateTree: function(),
+  ....
+
+}
